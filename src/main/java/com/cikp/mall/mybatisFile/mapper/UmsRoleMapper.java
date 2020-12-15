571e1fe4e44104ac457a7bb9b67a661ead7448de
chengyang.chen <chengyang.chen@tophant.com>
2020-12-15 20:10:37 +0800

    mapper

---
+++ src/main/java/com/cikp/mall/mybatisFile/mapper/UmsRoleMapper.java
@@ -0,0 +1,30 @@
+package com.cikp.mall.mybatisFile.mapper;
+
+import com.cikp.mall.mybatisFile.model.UmsRole;
+import com.cikp.mall.mybatisFile.model.UmsRoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsRoleMapper {
+    int countByExample(UmsRoleExample example);
+
+    int deleteByExample(UmsRoleExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsRole record);
+
+    int insertSelective(UmsRole record);
+
+    List<UmsRole> selectByExample(UmsRoleExample example);
+
+    UmsRole selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsRole record, @Param("example") UmsRoleExample example);
+
+    int updateByExample(@Param("record") UmsRole record, @Param("example") UmsRoleExample example);
+
+    int updateByPrimaryKeySelective(UmsRole record);
+
+    int updateByPrimaryKey(UmsRole record);
+}
